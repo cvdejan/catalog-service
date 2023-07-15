c8b639666adddcc753abd040af54761baa871b58
cvdejan <d_cvetkovski@yahoo.com>
2023-07-15 10:59:50 +1000

    fixing tests

--- src/test/java/io/cvdejan/catalogservice/domain/BookJsonTests.java
+++ src/test/java/io/cvdejan/catalogservice/domain/BookJsonTests.java
@@ -5,10 +5,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
 import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.boot.test.json.JsonContent;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @JsonTest
+@ActiveProfiles("production")
 class BookJsonTests {
 
     @Autowired
