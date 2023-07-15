3f62116a789fa8cfe5eef84ced6b70275b2b32bd
cvdejan <d_cvetkovski@yahoo.com>
2023-07-15 10:42:57 +1000

    added test containers

--- src/test/java/io/cvdejan/catalogservice/domain/CatalogServiceApplicationTest.java
+++ src/test/java/io/cvdejan/catalogservice/domain/CatalogServiceApplicationTest.java
@@ -3,11 +3,13 @@ package io.cvdejan.catalogservice.domain;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("integration")
 class CatalogServiceApplicationTest {
 
     @Autowired
