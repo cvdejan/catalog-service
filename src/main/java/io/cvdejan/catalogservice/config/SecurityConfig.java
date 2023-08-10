4775c20a461a589ce4ebcea2626bdea2e11e5540
cvdejan <d_cvetkovski@yahoo.com>
2023-08-10 13:59:39 +1000

    Added Observability

--- src/main/java/io/cvdejan/catalogservice/config/SecurityConfig.java
+++ src/main/java/io/cvdejan/catalogservice/config/SecurityConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
@@ -14,15 +13,14 @@ import org.springframework.security.web.SecurityFilterChain;
 
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.authorizeHttpRequests(authz->authz
-                    .requestMatchers(HttpMethod.GET,"/","/books/**").permitAll()
-                    .anyRequest().hasRole("employee")
-                    )
+        return http.authorizeHttpRequests(request->request
+                                .requestMatchers("/actuator/**").permitAll()
+                                .requestMatchers(HttpMethod.GET,"/","/books/**").permitAll()
+                                .anyRequest().hasRole("employee"))
                 .oauth2ResourceServer(configurer->configurer.jwt(Customizer.withDefaults()))
                 .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .csrf(AbstractHttpConfigurer::disable)
